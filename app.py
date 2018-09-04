d5ecf0ea12bac90ad045154d4fba5f1281027cb0
Ovelny <this.is.ovelny@gmail.com>
2018-09-04 18:38:03 +0200

    semantic changes

--- app.py
+++ app.py
@@ -15,9 +15,11 @@ freezer: Freezer = Freezer(app)
 
 @app.route("/")
 def home() -> Flask:
-    # ::: todo : sort posts by date :::
+    # :::| todo: sort posts by date |:::
     return render_template("index.html", pages=pages)
 
+# :::| todo: custom 404 page |:::
+# :::| todo: RSS stream |:::
 
 @app.route("/static/pygments.css")
 def pygments_css() -> pygments_style_defs:
@@ -25,6 +27,7 @@ def pygments_css() -> pygments_style_defs:
 
 
 @app.route("/<path:path>/")
+@app.route("/blog/<path:path>/")
 def page(path: str) -> Flask:
     page: Flask = pages.get_or_404(path)
     return render_template("page.html", page=page)
--- config.py
+++ config.py
@@ -9,18 +9,18 @@ def parent_dir(path: str) -> str:
     return os.path.abspath(os.path.join(path, os.pardir))
 
 
-# ::: paths & locations :::
+# :::| paths & locations |:::
 HERE: str = os.path.dirname(os.path.abspath(__file__))
 
-# ::: freezer config :::
+# :::| freezer config |:::
 REPO_NAME: str = "ovelblog"
 FREEZER_DESTINATION: str = HERE
 FREEZER_REMOVE_EXTRA_FILES: bool = False
 FREEZER_BASE_URL: str = "http://localhost/{0}".format(REPO_NAME)
 
-# ::: flatpages config :::
+# :::| flatpages config |:::
 DEBUG: bool = True
 FLATPAGES_EXTENSION: str = ".md"
 FLATPAGES_AUTO_RELOAD: bool = True
-FLATPAGES_ROOT: str = os.path.join(HERE, "pages")
+FLATPAGES_ROOT: str = os.path.join(HERE, "posts")
 FLATPAGES_MARKDOWN_EXTENSIONS: List[str] = ["codehilite"]

6e68ba098d8d24740c8da3012e7d5a4588ec30b6
Ovelny <this.is.ovelny@gmail.com>
2018-09-07 01:14:28 +0200

    rss feed ready to test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 import sys
 from flask_frozen import Freezer
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 from typing import Set, Dict, Tuple, List, Optional
 from flask_flatpages import FlatPages, pygments_style_defs
 
@@ -12,26 +12,33 @@ app.config.from_pyfile("config.py")
 pages: FlatPages = FlatPages(app)
 freezer: Freezer = Freezer(app)
 
-
+# :::| homepage with sorted articles |:::
 @app.route("/")
 def home() -> Flask:
     posts = [page for page in pages if "date" in page.meta]
     sorted_posts = sorted(posts, reverse=True, key=lambda page: page.meta["date"])
     return render_template("index.html", pages=sorted_posts)
 
-
-# :::| todo: custom 404 page |:::
+# :::| custom 404 page |:::
 @app.errorhandler(404)
-def page_not_found(e):
+def page_not_found(e: str) -> Flask:
     return render_template("404.html"), 404
-# :::| todo: RSS stream |:::
 
 
+# :::| RSS feed |:::
+@app.route("/atom.xml")
+def feed() -> Flask:
+    template = render_template("atom.xml", pages=pages)
+    response = make_response(template)
+    response.headers["Content-Type"] = "text/xml"
+    return response
+
+# :::| pygments css to render code highlighting |:::
 @app.route("/static/pygments.css")
 def pygments_css() -> pygments_style_defs:
     return pygments_style_defs("trac"), 200, {"Content-Type": "text/css"}
 
-
+# :::| articles and other pages rendering |:::
 @app.route("/<path:path>/")
 @app.route("/blog/<path:path>/")
 def page(path: str) -> Flask:
